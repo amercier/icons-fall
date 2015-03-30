38fc9a89e72847b8e6e36103c669dd49f0baf649
Alex Mercier <pro.alexandre.mercier@gmail.com>
2015-03-29 21:26:26 -0700

    Moved music to subfolder

--- src/app/music/music.controller.js
+++ src/app/music/music.controller.js
@@ -5,23 +5,23 @@ angular.module('iconsfall')
 
     $scope.albums = [{
       title: 'White Line',
-      cover: $sce.trustAsResourceUrl('http://files.iconsfall.com/white-line/cover.jpg'),
+      cover: $sce.trustAsResourceUrl('http://files.iconsfall.com/music/white-line/cover.jpg'),
       tracks: [
         {
           title: 'Golden Tree',
-          url: $sce.trustAsResourceUrl('http://files.iconsfall.com/white-line/01.%20Golden%20Tree.mp3')
+          url: $sce.trustAsResourceUrl('http://files.iconsfall.com/music/white-line/01.%20Golden%20Tree.mp3')
         },
         {
           title: 'Paths',
-          url: $sce.trustAsResourceUrl('http://files.iconsfall.com/white-line/02.%20Paths.mp3')
+          url: $sce.trustAsResourceUrl('http://files.iconsfall.com/music/white-line/02.%20Paths.mp3')
         },
         {
           title: 'Season Passed',
-          url: $sce.trustAsResourceUrl('http://files.iconsfall.com/white-line/03.%20Season%20Passed.mp3')
+          url: $sce.trustAsResourceUrl('http://files.iconsfall.com/music/white-line/03.%20Season%20Passed.mp3')
         },
         {
           title: 'White Lane',
-          url: $sce.trustAsResourceUrl('http://files.iconsfall.com/white-line/04.%20White%20Line.mp3')
+          url: $sce.trustAsResourceUrl('http://files.iconsfall.com/music/white-line/04.%20White%20Line.mp3')
         }
       ]
     }];
