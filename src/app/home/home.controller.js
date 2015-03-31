f6c4ee64e77cb47b495a9e8ed66a25711f84847b
Alex Mercier <pro.alexandre.mercier@gmail.com>
2015-03-30 23:42:24 -0700

    Fixed SVG logo issue

--- src/app/home/home.controller.js
+++ src/app/home/home.controller.js
@@ -4,6 +4,6 @@ angular.module('iconsfall')
   .controller('HomeCtrl', function ($scope, $http) {
     $http.get('/assets/images/iconsfall-logo.svg')
       .then(function(data) {
-        $('.home-logo').append($(data.data).get(2));
+        $('.home-logo').append($(data.data));
       });
   });
