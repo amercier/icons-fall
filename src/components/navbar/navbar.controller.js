a6fbf8d349985bbd472b72a68e4db77325662d47
Alex Mercier <pro.alexandre.mercier@gmail.com>
2015-04-03 00:36:45 -0700

    Moved topbar to main template

--- src/components/navbar/navbar.controller.js
+++ src/components/navbar/navbar.controller.js
@@ -1,6 +1,12 @@
 'use strict';
 
 angular.module('iconsfall')
-  .controller('NavbarCtrl', ['$scope', '$state', function ($scope, $state) {
-    $scope[$state.current.name] = $state.current;
+  .controller('NavbarCtrl', ['$scope', '$state', function ($scope) {
+    $scope.state = {};
+    $scope.$on('$stateChangeSuccess', function(e, toState) {
+      $scope.state = {}
+      $scope.state[toState.name] = toState;
+
+      $('body').attr('data-state', toState.name);
+    });
   }]);
