aa627893e7bc7f1bb0d9ea673749e797a5473a98
Alex Mercier <pro.alexandre.mercier@gmail.com>
2015-04-02 21:35:50 -0700

    Updated Icon's Fall logo

--- src/app/home/home.controller.js
+++ src/app/home/home.controller.js
@@ -2,8 +2,4 @@
 
 angular.module('iconsfall')
   .controller('HomeCtrl', function ($scope, $http) {
-    $http.get('/assets/images/iconsfall-logo.svg')
-      .then(function(data) {
-        $('.home-logo').append($(data.data));
-      });
   });
