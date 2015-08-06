dff61ba723f07a5043453340bf41bf91e5132205
Alex Mercier <pro.alexandre.mercier@gmail.com>
2015-08-05 23:38:10 -0700

    Added MusicController

--- src/app/index.route.js
+++ src/app/index.route.js
@@ -7,6 +7,12 @@ function routerConfig ($routeProvider) {
       controller: 'HomeController',
       controllerAs: 'home'
     })
+    .when('/music', {
+      id: 'music',
+      templateUrl: 'app/music/music.html',
+      controller: 'MusicController',
+      controllerAs: 'music'
+    })
     .otherwise({
       redirectTo: '/'
     });
