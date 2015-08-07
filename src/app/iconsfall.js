b9ee4efe35204d997839051c977a4e8d2359f0e0
Alex Mercier <pro.alexandre.mercier@gmail.com>
2015-08-06 21:47:14 -0700

    Added more Google Analytics timings

--- src/app/iconsfall.js
+++ src/app/iconsfall.js
@@ -28,11 +28,12 @@ angular.module('iconsfall', ['ngAnimate', 'ngTouch', 'ngResource', 'ui.router', 
   })
   .run(function($rootScope, $location) {
     var stateChangeStartTime;
+
     $rootScope.$on('$stateChangeStart', function() {
       stateChangeStartTime = Date.now();
     });
 
-    $rootScope.$on('$stateChangeSuccess', function() {
+    $rootScope.$on('$stateChangeSuccess', function(e) {
       setTimeout(function() {
         $rootScope.$apply(function () {
 
@@ -40,11 +41,55 @@ angular.module('iconsfall', ['ngAnimate', 'ngTouch', 'ngResource', 'ui.router', 
           $(document).foundation();
 
           // Track view on Google Analytics
-          if (window.ga) {
-            ga('send', 'pageview', { page: $location.path() });
-          }
-          gat('Angular', 'View load', stateChangeStartTime, $location.path());
+          ga('send', 'pageview', { page: $location.path() });
+          gat('View', 'ChangeSuccess', stateChangeStartTime, $location.path());
         });
       }, 0);
     });
+
+    $rootScope.$on('$viewContentLoading', function(e) {
+      gat('View', 'ContentLoading', stateChangeStartTime, $location.path());
+    });
+
+    $rootScope.$on('$viewContentLoaded', function(e) {
+      if (stateChangeStartTime) { // else: no change start
+        gat('View', 'ContentLoaded', stateChangeStartTime, $location.path());
+      }
+    });
   });
+
+// Error handling
+// --------------
+
+// JavaScript errors
+window.addEventListener('error', function (err) {
+  var lineAndColumnInfo = err.colno ? ' line:' + err.lineno +', column:'+ err.colno : ' line:' + err.lineno;
+  ga('send', 'event', 'JavaScript Error', err.message, err.filename + lineAndColumnInfo + ' -> ' +  navigator.userAgent, 0, true);
+});
+
+// AngularJS exceptions handler
+angular.module('iconsfall')
+  .config(function ($provide) {
+    $provide.decorator("$exceptionHandler", function ($delegate) {
+      return function (exception, cause) {
+        $delegate(exception, cause);
+        ga('send', 'event', 'AngularJS error', exception.message, exception.stack, 0, true);
+      };
+    });
+  });
+
+// jQuery errors
+jQuery.error = function (message) {
+  ga('send', 'event', 'jQuery Error', message, navigator.userAgent, 0, true);
+}
+
+// jQuery AJAX errors handler (jQuery API)
+$(document).ajaxError(function (event, request, settings) {
+  ga('send', 'event', 'jQuery Ajax Error', settings.url, JSON.stringify({
+    result: event.result,
+    status: request.status,
+    statusText: request.statusText,
+    crossDomain: settings.crossDomain,
+    dataType: settings.dataType
+  }), 0, true);
+});
