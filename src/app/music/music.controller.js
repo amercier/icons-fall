305490f4694e7cd5ccea78c7caf929c306db141f
Alex Mercier <pro.alexandre.mercier@gmail.com>
2015-08-06 20:37:58 -0700

    Fixed 'White Line' songs order

--- src/app/music/music.controller.js
+++ src/app/music/music.controller.js
@@ -12,15 +12,15 @@ angular.module('iconsfall')
           url: $sce.trustAsResourceUrl('http://files.iconsfall.com/music/white-line/01.%20Golden%20Tree.mp3')
         },
         {
-          title: 'Paths',
-          url: $sce.trustAsResourceUrl('http://files.iconsfall.com/music/white-line/02.%20Paths.mp3')
-        },
-        {
           title: 'Season Passed',
           url: $sce.trustAsResourceUrl('http://files.iconsfall.com/music/white-line/03.%20Season%20Passed.mp3')
         },
         {
-          title: 'White Lane',
+          title: 'Paths',
+          url: $sce.trustAsResourceUrl('http://files.iconsfall.com/music/white-line/02.%20Paths.mp3')
+        },
+        {
+          title: 'White Line',
           url: $sce.trustAsResourceUrl('http://files.iconsfall.com/music/white-line/04.%20White%20Line.mp3')
         }
       ]
