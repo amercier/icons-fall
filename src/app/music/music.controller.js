98f824972020aca41ed3c1caf66605455a4518c8
Alex Mercier <pro.alexandre.mercier@gmail.com>
2015-08-10 03:45:22 -0700

    Added ogg/webm support

--- src/app/music/music.controller.js
+++ src/app/music/music.controller.js
@@ -8,19 +8,27 @@ export default class MusicController {
       tracks: [
         {
           title: 'Golden Tree',
-          url: $sce.trustAsResourceUrl('http://files.iconsfall.com/music/white-line/01.%20Golden%20Tree.mp3')
+          ogg: $sce.trustAsResourceUrl('http://files.iconsfall.com/music/white-line/01.%20Golden%20Tree.ogg'),
+          mp3: $sce.trustAsResourceUrl('http://files.iconsfall.com/music/white-line/01.%20Golden%20Tree.mp3'),
+          webm: $sce.trustAsResourceUrl('http://files.iconsfall.com/music/white-line/01.%20Golden%20Tree.webm')
         },
         {
           title: 'Paths',
-          url: $sce.trustAsResourceUrl('http://files.iconsfall.com/music/white-line/02.%20Paths.mp3')
+          ogg: $sce.trustAsResourceUrl('http://files.iconsfall.com/music/white-line/02.%20Paths.ogg'),
+          mp3: $sce.trustAsResourceUrl('http://files.iconsfall.com/music/white-line/02.%20Paths.mp3'),
+          webm: $sce.trustAsResourceUrl('http://files.iconsfall.com/music/white-line/02.%20Paths.webm')
         },
         {
           title: 'Season Passed',
-          url: $sce.trustAsResourceUrl('http://files.iconsfall.com/music/white-line/03.%20Season%20Passed.mp3')
+          ogg: $sce.trustAsResourceUrl('http://files.iconsfall.com/music/white-line/03.%20Season%20Passed.ogg'),
+          mp3: $sce.trustAsResourceUrl('http://files.iconsfall.com/music/white-line/03.%20Season%20Passed.mp3'),
+          webm: $sce.trustAsResourceUrl('http://files.iconsfall.com/music/white-line/03.%20Season%20Passed.webm')
         },
         {
           title: 'White Lane',
-          url: $sce.trustAsResourceUrl('http://files.iconsfall.com/music/white-line/04.%20White%20Line.mp3')
+          ogg: $sce.trustAsResourceUrl('http://files.iconsfall.com/music/white-line/04.%20White%20Line.ogg'),
+          mp3: $sce.trustAsResourceUrl('http://files.iconsfall.com/music/white-line/04.%20White%20Line.mp3'),
+          webm: $sce.trustAsResourceUrl('http://files.iconsfall.com/music/white-line/04.%20White%20Line.webm')
         }
       ]
     }];
