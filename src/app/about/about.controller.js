d8b8d68bbe66e1b97c3e312452de9fb83dd00086
Icon's Fall CI <iconsfall@travis-ci.org>
2015-08-14 15:49:03 +0000

    Update 2015-08-14T15:49:02.625Z

--- src/app/about/about.controller.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-angular.module('iconsfall')
-  .controller('AboutCtrl', function ($rootScope, MetaService) {
-    $rootScope.meta = MetaService;
-    $rootScope.meta.setPageTitle('Bio');
-  });
--- src/app/home/home.controller.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-angular.module('iconsfall')
-  .controller('HomeCtrl', function ($rootScope, MetaService) {
-    $rootScope.meta = MetaService;
-    $rootScope.meta.setPageTitle(false);
-  });
