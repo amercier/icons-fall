757d8f8a89f861ae87e56369f19055174cce0732
Alex Mercier <pro.alexandre.mercier@gmail.com>
2015-08-17 23:35:44 -0700

    Fixed navbar active links

--- src/components/navbar/navbar.controller.js
+++ src/components/navbar/navbar.controller.js
@@ -1,11 +1,11 @@
 'use strict';
 
 angular.module('iconsfall')
-  .controller('NavbarController', ['$scope', '$state', function ($scope) {
-    $scope.state = {};
-    $scope.$on('$stateChangeSuccess', function(e, toState) {
-      $scope.state = {};
-      $scope.state[toState.name] = toState;
+  .controller('NavbarController', function ($rootScope) {
+    $rootScope.state = {};
+    $rootScope.$on('$stateChangeSuccess', function(e, toState) {
+      $rootScope.state = {};
+      $rootScope.state[toState.name] = toState;
       angular.element(document.body).attr('data-state', toState.name);
     });
-  }]);
+  });
