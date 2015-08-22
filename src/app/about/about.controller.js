270a662672b2e2ecb6eba0c71ebb06777839d1f0
Alex Mercier <pro.alexandre.mercier@gmail.com>
2015-08-21 19:20:04 -0700

    Deleted all files

--- src/app/about/about.controller.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-angular.module('iconsfall')
-  .controller('AboutController', function ($rootScope, MetaService) {
-    $rootScope.meta = MetaService;
-    $rootScope.meta.setPageTitle('Bio');
-  });
--- src/app/home/home.controller.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-angular.module('iconsfall')
-  .controller('HomeController', function ($rootScope, MetaService) {
-    $rootScope.meta = MetaService;
-    $rootScope.meta.setPageTitle(false);
-  });
