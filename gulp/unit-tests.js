c945b567a56a0747b86a2f3bb2f9f851d8eefd8b
Alex Mercier <pro.alexandre.mercier@gmail.com>
2016-03-18 11:13:18 -0700

    Updated 'gulp test' task to new Karma interface

--- gulp/unit-tests.js
+++ gulp/unit-tests.js
@@ -4,16 +4,17 @@ var path = require('path');
 var gulp = require('gulp');
 var conf = require('./conf');
 
-var karma = require('karma');
+var Server = require('karma').Server;
 
 function runTests (singleRun, done) {
-  karma.server.start({
+  var server = new Server({
     configFile: path.join(__dirname, '/../karma.conf.js'),
     singleRun: singleRun,
     autoWatch: !singleRun
   }, function(failCount) {
     done(failCount ? new Error("Failed " + failCount + " tests.") : null);
   });
+  server.start();
 }
 
 gulp.task('test', ['scripts'], function(done) {
