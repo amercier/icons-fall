59495b9b6b8217270089a055eba2c552a88a107e
Alex Mercier <pro.alexandre.mercier@gmail.com>
2017-08-16 15:45:19 +0200

    Fixed security warning in SSL

--- src/app/music/discography.service.js
+++ src/app/music/discography.service.js
@@ -5,46 +5,46 @@ export default class DiscographyService {
     this.albums = [{
       title: 'White Line',
       type: 'EP',
-      cover: $sce.trustAsResourceUrl('http://cdn.iconsfall.com/music/white-line/cover.jpg'),
+      cover: $sce.trustAsResourceUrl('//cdn.iconsfall.com/music/white-line/cover.jpg'),
       tracks: [
         {
           track: '01',
           title: 'Golden Tree',
           sources: [
-            { src: $sce.trustAsResourceUrl('http://cdn.iconsfall.com/music/white-line/01.%20Golden%20Tree.ogg'), type: 'audio/ogg; codecs="vorbis"' },
-            { src: $sce.trustAsResourceUrl('http://cdn.iconsfall.com/music/white-line/01.%20Golden%20Tree.webm'), type: 'audio/webm; codecs="vorbis"' },
-            { src: $sce.trustAsResourceUrl('http://cdn.iconsfall.com/music/white-line/01.%20Golden%20Tree.m4a'), type: 'audio/mp4' },
-            { src: $sce.trustAsResourceUrl('http://cdn.iconsfall.com/music/white-line/01.%20Golden%20Tree.mp3'), type: 'audio/mp3' }
+            { src: $sce.trustAsResourceUrl('//cdn.iconsfall.com/music/white-line/01.%20Golden%20Tree.ogg'), type: 'audio/ogg; codecs="vorbis"' },
+            { src: $sce.trustAsResourceUrl('//cdn.iconsfall.com/music/white-line/01.%20Golden%20Tree.webm'), type: 'audio/webm; codecs="vorbis"' },
+            { src: $sce.trustAsResourceUrl('//cdn.iconsfall.com/music/white-line/01.%20Golden%20Tree.m4a'), type: 'audio/mp4' },
+            { src: $sce.trustAsResourceUrl('//cdn.iconsfall.com/music/white-line/01.%20Golden%20Tree.mp3'), type: 'audio/mp3' }
           ]
         },
         {
           track: '02',
           title: 'Season Passed',
           sources: [
-            { src: $sce.trustAsResourceUrl('http://cdn.iconsfall.com/music/white-line/02.%20Season%20Passed.ogg'), type: 'audio/ogg; codecs="vorbis"' },
-            { src: $sce.trustAsResourceUrl('http://cdn.iconsfall.com/music/white-line/02.%20Season%20Passed.webm'), type: 'audio/webm; codecs="vorbis"' },
-            { src: $sce.trustAsResourceUrl('http://cdn.iconsfall.com/music/white-line/02.%20Season%20Passed.m4a'), type: 'audio/mp4' },
-            { src: $sce.trustAsResourceUrl('http://cdn.iconsfall.com/music/white-line/02.%20Season%20Passed.mp3'), type: 'audio/mp3' }
+            { src: $sce.trustAsResourceUrl('//cdn.iconsfall.com/music/white-line/02.%20Season%20Passed.ogg'), type: 'audio/ogg; codecs="vorbis"' },
+            { src: $sce.trustAsResourceUrl('//cdn.iconsfall.com/music/white-line/02.%20Season%20Passed.webm'), type: 'audio/webm; codecs="vorbis"' },
+            { src: $sce.trustAsResourceUrl('//cdn.iconsfall.com/music/white-line/02.%20Season%20Passed.m4a'), type: 'audio/mp4' },
+            { src: $sce.trustAsResourceUrl('//cdn.iconsfall.com/music/white-line/02.%20Season%20Passed.mp3'), type: 'audio/mp3' }
           ]
         },
         {
           track: '03',
           title: 'Paths',
           sources: [
-            { src: $sce.trustAsResourceUrl('http://cdn.iconsfall.com/music/white-line/03.%20Paths.ogg'), type: 'audio/ogg; codecs="vorbis"' },
-            { src: $sce.trustAsResourceUrl('http://cdn.iconsfall.com/music/white-line/03.%20Paths.webm'), type: 'audio/webm; codecs="vorbis"' },
-            { src: $sce.trustAsResourceUrl('http://cdn.iconsfall.com/music/white-line/03.%20Paths.m4a'), type: 'audio/mp4' },
-            { src: $sce.trustAsResourceUrl('http://cdn.iconsfall.com/music/white-line/03.%20Paths.mp3'), type: 'audio/mp3' }
+            { src: $sce.trustAsResourceUrl('//cdn.iconsfall.com/music/white-line/03.%20Paths.ogg'), type: 'audio/ogg; codecs="vorbis"' },
+            { src: $sce.trustAsResourceUrl('//cdn.iconsfall.com/music/white-line/03.%20Paths.webm'), type: 'audio/webm; codecs="vorbis"' },
+            { src: $sce.trustAsResourceUrl('//cdn.iconsfall.com/music/white-line/03.%20Paths.m4a'), type: 'audio/mp4' },
+            { src: $sce.trustAsResourceUrl('//cdn.iconsfall.com/music/white-line/03.%20Paths.mp3'), type: 'audio/mp3' }
           ]
         },
         {
           track: '04',
           title: 'White Line',
           sources: [
-            { src: $sce.trustAsResourceUrl('http://cdn.iconsfall.com/music/white-line/04.%20White%20Line.ogg'), type: 'audio/ogg; codecs="vorbis"' },
-            { src: $sce.trustAsResourceUrl('http://cdn.iconsfall.com/music/white-line/04.%20White%20Line.webm'), type: 'audio/webm; codecs="vorbis"' },
-            { src: $sce.trustAsResourceUrl('http://cdn.iconsfall.com/music/white-line/04.%20White%20Line.m4a'), type: 'audio/mp4' },
-            { src: $sce.trustAsResourceUrl('http://cdn.iconsfall.com/music/white-line/04.%20White%20Line.mp3'), type: 'audio/mp3' }
+            { src: $sce.trustAsResourceUrl('//cdn.iconsfall.com/music/white-line/04.%20White%20Line.ogg'), type: 'audio/ogg; codecs="vorbis"' },
+            { src: $sce.trustAsResourceUrl('//cdn.iconsfall.com/music/white-line/04.%20White%20Line.webm'), type: 'audio/webm; codecs="vorbis"' },
+            { src: $sce.trustAsResourceUrl('//cdn.iconsfall.com/music/white-line/04.%20White%20Line.m4a'), type: 'audio/mp4' },
+            { src: $sce.trustAsResourceUrl('//cdn.iconsfall.com/music/white-line/04.%20White%20Line.mp3'), type: 'audio/mp3' }
           ]
         }
       ]
